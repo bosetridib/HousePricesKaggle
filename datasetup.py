26b6bdb976f27c1665b735440eee50e45da67fdc
bosetridib <bosetridib@yahoo.com>
2023-03-18 19:43:29 +0530

    ...

--- datasetup.py
+++ datasetup.py
@@ -1,5 +1,12 @@
+# The Data-Setup for house price prediction, from
+# kaggle's House prices competition.
+
 import pandas as pd
 import seaborn as sns
-import ydata_profiling as yd
 
+# Load the train and test data.
 train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+train.apply(len)
+test.apply(len)
