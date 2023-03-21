c1c1bd2bf4e4c05bc937e2be9515cd6e05934a34
bosetridib <bosetridib@yahoo.com>
2023-03-21 18:50:41 +0530

    ...

--- datasetup.py
+++ datasetup.py
@@ -3,10 +3,35 @@
 
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load the train and test data.
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-train.apply(len)
-test.apply(len)
+# The sahpe shows both train and test have same
+# number of observations and features.
+print(train.shape)
+print(test.shape)
+
+# Let us check for NaN values.
+
+missing_train = train.isnull().sum()[train.isnull().sum() != 0]
+missing_test = test.isnull().sum()[test.isnull().sum() != 0]
+
+fig, axis = plt.subplots(1,2)
+fig.suptitle("Missing values distribution in test and train")
+
+sns.violinplot(missing_train, ax=axis[0])
+sns.boxplot(ax=axis[1], data=missing_test)
+plt.show()
+
+# Let us check the mutual information scores and
+# VIF of each feature.
+
+from sklearn.metrics import mutual_info_score
+
+X_train = train.drop(columns='SalePrice')
+Y_train = train['SalePrice']
+
+mutual_info_score(X_train, Y_train)
