dd5ba141ecb3232ebfd4410b9b64dd1eea3672ae
bosetridib <bosetridib@yahoo.com>
2023-03-24 01:04:37 +0530

    ...

--- datasetup.py
+++ datasetup.py
@@ -20,13 +20,28 @@ missing_train = train.isnull().sum()[train.isnull().sum() != 0]
 missing_test = test.isnull().sum()[test.isnull().sum() != 0]
 
 fig, axis = plt.subplots(1,2)
-fig.suptitle("Missing values distribution in test and train")
-sns.barplot(x = missing_train.index, y = missing_train.values, ax=axis[0])
+fig.suptitle("Missing values in test and train")
+
+sns.barplot(
+    x = missing_train.index,
+    y = missing_train.values,
+    ax=axis[0],     # The left side
+    palette="bright"
+)
 axis[0].set_title("Train")
 axis[0].tick_params(axis = 'x', rotation=90)
-sns.barplot(x = missing_test.index, y = missing_test.values, ax=axis[0])
+axis[0].bar_label(axis[0].containers[0])
+
+sns.barplot(
+    x = missing_test.index,
+    y = missing_test.values,
+    ax=axis[1],     # The right side
+    palette="dark"
+)
 axis[1].set_title("Test")
 axis[1].tick_params(axis = 'x', rotation=90)
+axis[1].bar_label(axis[1].containers[0])
+
 plt.show()
 
 # Let us check the mutual information scores and
