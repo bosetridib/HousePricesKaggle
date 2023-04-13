da98e3fe6d300346a1df1ecf51bb1a03e6ef2d87
bosetridib <bosetridib@yahoo.com>
2023-04-13 19:37:46 +0530

    ...

--- mlapps.py
+++ mlapps.py
@@ -3,10 +3,13 @@ print(vif_train)
 print(mi_train_score)
 
 X_train = train.drop(columns='SalePrice')
-Y_train = train[['SalePrice']]
+Y_train = train['SalePrice']
 
-from statsmodels.api import Logit
-X_train_logit = X_train.select_dtypes(['int64','float64'])
-logit_model = Logit(Y_train, X_train_logit).fit()
+from sklearn.ensemble import RandomForestRegressor
+random_forest_model = RandomForestRegressor(n_estimators=200, max_depth=4, random_state=0)
+random_forest_model.fit(X_train.select_dtypes(['int64', 'float64']), Y_train.values)
+Y_test = random_forest_model.predict(test.select_dtypes(['int64', 'float64']))
 
-logit_model(X_train_logit,Y_train)
+Y_test = pd.DataFrame(Y_test, index=test.index, columns=['SalePrice'])
+Y_test.to_csv('submission.csv')
+
