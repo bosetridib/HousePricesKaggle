1f2d9e31a45fcde4e9d9dca3a58e0335cb19efb1
bosetridib <bosetridib@yahoo.com>
2023-05-02 19:18:47 +0530

    ...

--- mlapps.py
+++ mlapps.py
@@ -4,6 +4,7 @@ print(mi_train_score[mi_train_score<2])
 
 X_train = train.drop(columns='SalePrice')
 Y_train = train['SalePrice']
+X_test = test.copy()
 
 from sklearn.ensemble import RandomForestRegressor
 random_forest_model = RandomForestRegressor(n_estimators=200, max_depth=4, random_state=0)
@@ -74,15 +75,68 @@ Y_test = pd.DataFrame(Y_test, index=test.index, columns=['SalePrice'])
 Y_test.to_csv('submission.csv')
 
 
-# One-hot encoding
+
+
+# ------------------------ One-hot encoding ------------------------ #
 
 # The more top frequency closer to the total frequency, the more the variable is useless.
 print(X_train.select_dtypes(['category']).describe().iloc[:,:10])
 X_train.drop(columns=['Street','Utilities','Condition2'], inplace=True)
+X_test.drop(columns=['Street','Utilities','Condition2'], inplace=True)
 print(X_train.select_dtypes(['category']).describe().iloc[:,10:20])
 X_train.drop(columns=['RoofMatl'], inplace=True)
+X_test.drop(columns=['RoofMatl'], inplace=True)
 print(X_train.select_dtypes(['category']).describe().iloc[:,20:30])
 X_train.drop(columns=['Heating'], inplace=True)
+X_test.drop(columns=['Heating'], inplace=True)
 print(X_train.select_dtypes(['category']).describe().iloc[:,30:])
 
-X_train_encoded = pd.get_dummies(X_train['ExterQual']).astype('int')
+X_train_encoded = X_train.select_dtypes(['int64', 'float64'])
+for i in X_train.select_dtypes('category').columns:
+    X_train_encoded = pd.concat(
+        [X_train_encoded, pd.get_dummies(X_train[i], prefix=i).astype('int')],
+        axis=1
+    )
+
+X_test_encoded = test.select_dtypes(['int64','float64'])
+for i in X_test.select_dtypes('category').columns:
+    X_test_encoded = pd.concat(
+        [X_test_encoded, pd.get_dummies(X_test[i], prefix=i).astype('int')],
+        axis=1
+    )
+
+# Remove different columns in the one-hot encoded train and test datasets
+
+for i,j in zip(X_train_encoded.columns, X_test_encoded.columns):
+    if i not in X_test_encoded.columns:
+        X_train_encoded.drop(columns=i, inplace=True)
+    if j not in X_train_encoded.columns:
+        X_test_encoded.drop(columns=j, inplace=True)
+
+from sklearn.ensemble import GradientBoostingRegressor
+rmse = []
+
+for i,j,k in [(i,j,k) for i in range(100,3100,500) for j in range(1,21,4) for k in range(3,21,6)]:
+    
+    gradient_boost_model = GradientBoostingRegressor(n_estimators=i, max_depth=j, min_samples_leaf=k)
+    gradient_boost_model.fit(X_train_encoded, Y_train.values)
+    
+    Y_train_est = gradient_boost_model.predict(X_train_encoded)
+    
+    rmse.append(
+        {
+            'rmse':(sum( (Y_train.values - Y_train_est)**2 ) / len(Y_train))**0.5,
+            'i':i, 'j':j, 'k':k
+        }
+    )
+
+rmse = pd.DataFrame(rmse)
+[[r,i,j,k]] = rmse[rmse['rmse'] == rmse['rmse'].min()].values.tolist()
+i,j,k = int(i),int(j),int(k)
+gradient_boost_model = GradientBoostingRegressor(n_estimators=i, max_depth=j, max_leaf_nodes=k)
+del rmse,r,i,j,k
+gradient_boost_model.fit(X_train_encoded, Y_train.values)
+Y_test = gradient_boost_model.predict(X_test_encoded)
+
+Y_test = pd.DataFrame(Y_test, index=test.index, columns=['SalePrice'])
+Y_test.to_csv('submission.csv')
